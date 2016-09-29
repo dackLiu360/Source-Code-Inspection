e867b8bcdcf2db13431ba76f114d773a3060c973
31437974 <lucas.live.sds@hotmail.com>
2016-09-29 11:49:20 -0300

    Teste

--- Source Code Inspection/test/Test01.java
+++ Source Code Inspection/test/Test01.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 
+import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,14 +23,23 @@ import static org.junit.Assert.*;
 public class Test01 {
     
     public Test01() {
-    
+        
+        
+        
+        
+        
+        
     }
     
     @Test
-    public void hello() {
-    int a = 10;
-    int b = 10;
-    
-    Assert.assertEquals(b, a);
+    public void hello() {      
+        TicketMachine ticket = new TicketMachine();
+        try {
+            double resultado;
+            ticket.inserir(20);
+    Assert.assertEquals(20, ticket.getSaldo());
+        } catch (PapelMoedaInvalidaException ex) {
+            Logger.getLogger(Test01.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
